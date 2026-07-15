df0a9b83476030e9331b8397d4a4fb238ebb909f
Unzila Raees <unzilaraees595@gmail.com>
2026-07-15 23:40:39 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+print("=" * 40)
+print("   Employee Management System")
+print("=" * 40)
+
+print("\nWelcome to Employee Management System")
+
+print("\nFeatures:")
+print("1. Add Employee")
+print("2. View Employees")
+print("3. Search Employee")
+print("4. Update Employee")
+print("5. Delete Employee")
+print("6. Exit")
+
+print("\nProject is currently under development...")
